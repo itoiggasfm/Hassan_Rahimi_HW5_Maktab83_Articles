964b04018e7c6fddf9d5223529fe4029e279e186
hrahimi <hassan.rahimi@live.com>
2022-09-19 10:49:23 +0430

    Database and tables created. Postgres depenedency added to pom.xml file. connection package and database connection and config files added.

---
+++ src/main/java/connection/DBConfig.java
@@ -0,0 +1,9 @@
+package connection;
+
+public class DBConfig {
+
+        public static final String url = "jdbc:postgresql://localhost/Article";
+        public static final String user = "postgres";
+        public static final String password = "99497";
+
+}
