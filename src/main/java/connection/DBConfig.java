8a5689c554da0d3634837b7bee11ae1eed694b85
hrahimi <hassan.rahimi@live.com>
2022-09-19 15:58:27 +0430

    User sign in and sign up completed.

--- src/main/java/connection/DBConfig.java
+++ src/main/java/connection/DBConfig.java
@@ -2,7 +2,7 @@ package connection;
 
 public class DBConfig {
 
-        public static final String url = "jdbc:postgresql://localhost/Article";
+        public static final String url = "jdbc:postgresql://localhost/article";
         public static final String user = "postgres";
         public static final String password = "99497";
 
